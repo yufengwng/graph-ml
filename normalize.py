a4f71164e76e11c3a4ae087da206465f10d94211
Yufeng Wang <yufengwang05@gmail.com>
2021-10-12 14:57:02 -0500

    gcn: check-in dataset

---
+++ normalize.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+
+cites = pd.read_csv('~/dataset/cora/cites.csv')
+labels = pd.read_csv('~/dataset/cora/paper.csv')
+content = pd.read_csv('~/dataset/cora/content.csv')
+
+labels.sort_values('paper_id', inplace=True)
+papers = labels['paper_id']
+
+unique_words = content['word_cited_id'].unique()
+unique_words.sort()
+
+rows = []
+for id in papers:
+    matches = content[content['paper_id'] == id]
+    mentions = set(matches['word_cited_id'])
+    row = [id]
+    for word in unique_words:
+        row.append(1 if word in mentions else 0)
+    rows.append(row)
+columns = ['paper_id'] + list(unique_words)
+nodes = pd.DataFrame(np.array(rows), columns=columns)
+
+edges = cites.reindex(columns=['citing_paper_id', 'cited_paper_id'])
+edges.sort_values('citing_paper_id', inplace=True)
+
+nodes.to_csv('data/cora-nodes.csv', index=False)
+edges.to_csv('data/cora-edges.csv', index=False)
+labels.to_csv('data/cora-labels.csv', index=False)
